1120428276c359d1e3b3ad79453d7f1679f203c0
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-04-30 12:56:17 +0600

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u8jfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -10,17 +13,21 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+console.log(uri);
+console.log("start the route");
 async function run() {
   try {
     await client.connect();
     const inventoryItemCollection = client
       .db("InventoryItem")
       .collection("Items");
+    console.log("into the try");
 
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = inventoryItemCollection.find(query);
       const result = await cursor.toArray();
+      console.log("into the products");
       console.log(result);
       res.send(result);
     });
@@ -28,6 +35,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+console.log("end the route");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
