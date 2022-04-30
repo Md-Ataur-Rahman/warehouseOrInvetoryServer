fd584fc72f1b3c6d9c5b622e6175585ad37651da
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-01 01:05:03 +0600

    update system

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u8jfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -13,29 +14,47 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
-console.log("start the route");
 async function run() {
   try {
     await client.connect();
     const inventoryItemCollection = client
       .db("InventoryItem")
       .collection("Items");
-    console.log("into the try");
 
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = inventoryItemCollection.find(query);
       const result = await cursor.toArray();
-      console.log("into the products");
-      console.log(result);
+      res.send(result);
+    });
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryItemCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const quantity = await body.quantity;
+      console.log(quantity);
+      const filter = { _id: ObjectId(id) };
+      const updateQuantity = {
+        $set: {
+          quantity,
+        },
+      };
+      const result = await inventoryItemCollection.updateOne(
+        filter,
+        updateQuantity
+      );
       res.send(result);
     });
   } finally {
   }
 }
 run().catch(console.dir);
-console.log("end the route");
 
 app.get("/", (req, res) => {
   res.send("Hello World!, store dress");
